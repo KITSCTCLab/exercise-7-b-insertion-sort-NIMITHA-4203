e8fe3196f92d586dbd47f5763cf260ff8ad643a8
NIMITHA <101205074+NIMITHA-4203@users.noreply.github.com>
2022-09-12 15:28:03 +0530

    Second Commit

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ def insertionSort(array) -> List[int]:
   for index in range(1, len(array)):
     key = array[index]    
     indexo = index - 1
-    while indexo >= o and key < array[indexo]:
+    while indexo >= 0 and key < array[indexo]:
       array[indexo + 1] =  array[indexo]
       indexo -= 1
     array[indexo + 1] = key
